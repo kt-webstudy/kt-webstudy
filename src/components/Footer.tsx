d842ffab100b56844f90014a6872254b4ce38aae
CSBL at Korea University <igchoi@korea.ac.kr>
2024-12-27 17:34:24 +0900

    Update Footer.tsx
    Footer 라인조정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,19 +9,14 @@ const Footer: React.FC = () => {
         <img src={logo} alt="Korea University Logo" className={styles.logo} />
         <div className={styles.info}>
           <p>
-            [02841] 서울시 성북구 안암로 145 고려대학교 우당교양관 5층 501호
-            학부대학
-          </p>
-          <p>Tel: 02-3290-1591~9 &nbsp;&nbsp;&nbsp;&nbsp; FAX: 02-921-0323</p>
+            [02841] 서울시 성북구 안암로 145 고려대학교 우당교양관 5층 501호 학부대학 Tel: 02-3290-1591~9 &nbsp;&nbsp;&nbsp;&nbsp; FAX: 02-921-0323</p>
           <hr className={styles.divider} />
           <p>
-            개인정보처리방침 &nbsp;&nbsp;&nbsp;&nbsp; Copyright © Korea
-            University. All Rights Reserved
+            개인정보처리방침 &nbsp;&nbsp;&nbsp;&nbsp; Copyright © Korea University. All Rights Reserved
           </p>
           <div className={styles.Contributed}>
             <p>
-              본 페이지는 학부대학 근로장학생 박승현, 신종현, 김규탁 학생의
-              참여로 제작되었습니다.
+              본 페이지는 학부대학 근로장학생 박승현(웹페이지 디자인/제작), 신종현(데이터표준화), 김규탁(데이터표준화)에 의해 오픈소스로 제작되었습니다.
             </p>
           </div>
         </div>
